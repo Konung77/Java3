bcaa73490e91696f0e592467a70b302bc317e9e9
Konung77 <konungr@mail.ru>
2019-03-24 18:05:16 +0300

    Очистка ветки

--- src/lesson1/Apple.java
+++
@@ -1,7 +0,0 @@
-package lesson1;
-
-public class Apple extends Fruit {
-    public Apple() {
-        weight = 1.0f;
-    }
-}
--- src/lesson1/Orange.java
+++
@@ -1,7 +0,0 @@
-package lesson1;
-
-public class Orange extends Fruit {
-    public Orange() {
-        weight = 1.5f;
-    }
-}
