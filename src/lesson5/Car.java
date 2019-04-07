c38f835fbb265fb5464207b49a2719f238dc9488
Konung77 <konungr@mail.ru>
2019-04-08 00:20:15 +0300

    Изменения

--- src/lesson5/Car.java
+++ src/lesson5/Car.java
@@ -1,5 +1,7 @@
 package lesson5;
 
+import java.util.concurrent.CountDownLatch;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
     static {
@@ -8,25 +10,37 @@ public class Car implements Runnable {
     private Race race;
     private int speed;
     private String name;
+    CountDownLatch cdl = null;
+
     public String getName() {
         return name;
     }
+
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+
+    public Car(Race race, int speed, int mcCarsCount) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+//        cdl = new CountDownLatch(mcCarsCount);
     }
+
     @Override
     public void run() {
         try {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
+            MainClass.cdl.countDown();
             System.out.println(this.name + " готов");
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            MainClass.cdl.await();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
         for (int i = 0; i < race.getStages().size(); i++) {
