4221bcc88e325437b3037bc5460c9015aa665406
Konung77 <konungr@mail.ru>
2019-04-10 14:28:18 +0300

    Домашняя работа к 5 уроку

--- src/lesson5/Car.java
+++ src/lesson5/Car.java
@@ -10,7 +10,14 @@ public class Car implements Runnable {
     private Race race;
     private int speed;
     private String name;
-    CountDownLatch cdl = null;
+    private static int readyCount;
+    static {
+        readyCount = 0;
+    }
+    private static int finishCount;
+    static {
+        finishCount = 0;
+    }
 
     public String getName() {
         return name;
@@ -25,7 +32,6 @@ public class Car implements Runnable {
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
-//        cdl = new CountDownLatch(mcCarsCount);
     }
 
     @Override
@@ -33,18 +39,19 @@ public class Car implements Runnable {
         try {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
-            MainClass.cdl.countDown();
+            MainClass.ready = true;
             System.out.println(this.name + " готов");
+            readyCount++;
+            if (readyCount == CARS_COUNT) System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+            MainClass.cb.await();
         } catch (Exception e) {
-            e.printStackTrace();
-        }
-        try {
-            MainClass.cdl.await();
-        } catch (InterruptedException e) {
             e.printStackTrace();
         }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+        finishCount++;
+        if (finishCount == 1) System.out.println(this.name + " - WIN!!!");
+        else if (finishCount == CARS_COUNT) System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
--- src/lesson5/MainClass.java
+++ src/lesson5/MainClass.java
@@ -1,23 +1,23 @@
 package lesson5;
 
-import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 
 public class MainClass {
     public static final int CARS_COUNT = 4;
-    static CountDownLatch cdl = null;
+    public static boolean ready;
+    static CyclicBarrier cb = null;
 
     public static void main(String[] args) {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
-        cdl = new CountDownLatch(CARS_COUNT);
+        cb = new CyclicBarrier(CARS_COUNT);
+        ready = false;
         for (int i = 0; i < cars.length; i++) {
             cars[i] = new Car(race, 20 + (int) (Math.random() * 10), CARS_COUNT);
         }
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
