ea15f861d2d6cfebe20e32be2e9415f782c0dac3
Konung77 <konungr@mail.ru>
2019-04-13 00:15:13 +0300

    Домашняя работа к 6 уроку

--- src/lesson6/Main6.java
+++ src/lesson6/Main6.java
@@ -1,7 +1,5 @@
 package lesson6;
 
-import java.util.Arrays;
-
 public class Main6 {
     public static int[] exclude (int[] array)
     {
@@ -25,39 +23,5 @@ public class Main6 {
             if ((array[i] == 1) || (array[i] == 4)) return true;
         }
         return false;
-    }
-
-    public static void main(String[] args) {
-        int[] array1 = {0,1,2,3,4,5,6,7,8};
-        int[] array2 = {1,2,4,4,2,3,4,1,7};
-        int[] array3 = {2,3,5,6,7};
-
-        System.out.println(Arrays.toString(array1));
-        try {
-            System.out.println(Arrays.toString(exclude(array1)));
-        }
-        catch (RuntimeException e)
-        {
-            System.out.println("В массиве нет четверок");
-        }
-        System.out.println(is1or4(array1)?"В массиве есть 1 или 4":"В массиве нет ни 1 ни 4");
-        System.out.println(Arrays.toString(array2));
-        try {
-            System.out.println(Arrays.toString(exclude(array2)));
-        }
-        catch (RuntimeException e)
-        {
-            System.out.println("В массиве нет четверок");
-        }
-        System.out.println(is1or4(array2)?"В массиве есть 1 или 4":"В массиве нет ни 1 ни 4");
-        System.out.println(Arrays.toString(array3));
-        try {
-            System.out.println(Arrays.toString(exclude(array3)));
-        }
-        catch (RuntimeException e)
-        {
-            System.out.println("В массиве нет четверок");
-        }
-        System.out.println(is1or4(array3)?"В массиве есть 1 или 4":"В массиве нет ни 1 ни 4");
     }
 }
